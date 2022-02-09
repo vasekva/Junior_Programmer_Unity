8a62bb8000ffff9bebdca02f2588ed0fa9bb5e34
vasekva <vahonin_vasek@mail.ru>
2022-02-09 17:12:53 +0300

    Made first playable version of the game with cars + (Added realistic car behavior)

---
+++ Prototype_1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private float verRotationSpeed = 100.0f;
+    private float horRotationSpeed = 100.0f;
+    private float verticalInput;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+        transform.Rotate(Vector3.right, Time.deltaTime * verRotationSpeed * verticalInput);
+        transform.Rotate(new Vector3(0, 0, -1), Time.deltaTime * horRotationSpeed * horizontalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        // transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Prototype_1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using YamlDotNet.Core.Tokens;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    
+    // private float speed = 20;
+    // private float turnSpeed = 25;
+    // private float horizontalInput;
+    // private float verticalInput;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteeringAngle;
+    private float currentBreakForce;
+    private bool isBreaking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteeringAngle;
+    
+    [SerializeField] private WheelCollider fl_wheelCollider;
+    [SerializeField] private WheelCollider fr_wheelCollider;
+    [SerializeField] private WheelCollider rl_wheelCollider;
+    [SerializeField] private WheelCollider rr_wheelCollider;
+
+    [SerializeField] private Transform fl_wheelTransform;
+    [SerializeField] private Transform fr_wheelTransform;
+    [SerializeField] private Transform rl_wheelTransform;
+    [SerializeField] private Transform rr_wheelTransform;
+    
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+        // horizontalInput = Input.GetAxis("Horizontal");
+        // verticalInput = Input.GetAxis("Vertical");
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(fl_wheelCollider, fl_wheelTransform);
+        UpdateSingleWheel(fr_wheelCollider, fr_wheelTransform);
+        UpdateSingleWheel(rl_wheelCollider, rl_wheelTransform);
+        UpdateSingleWheel(rr_wheelCollider, rr_wheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteeringAngle = maxSteeringAngle * horizontalInput;
+        fl_wheelCollider.steerAngle = currentSteeringAngle;
+        fr_wheelCollider.steerAngle = currentSteeringAngle;
+    }
+
+    private void HandleMotor()
+    {
+        fl_wheelCollider.motorTorque = verticalInput * motorForce;
+        fr_wheelCollider.motorTorque = verticalInput * motorForce;
+        currentBreakForce = isBreaking ? breakForce : 0f;
+        if (isBreaking)
+        {
+            ApplyBreaking();
+        }
+    }
+
+    private void ApplyBreaking()
+    {
+        fl_wheelCollider.brakeTorque = currentBreakForce;
+        fr_wheelCollider.brakeTorque = currentBreakForce;
+        rl_wheelCollider.brakeTorque = currentBreakForce;
+        rr_wheelCollider.brakeTorque = currentBreakForce;
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+}
